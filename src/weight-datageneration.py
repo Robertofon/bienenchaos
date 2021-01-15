ca3641641f7166140f9f172c34186c5cd370e02c
kolossos <alder_tim@hotmail.com>
2021-01-15 19:53:47 +0100

    +hiveeeyes

--- src/weight-datageneration.py
+++ src/weight-datageneration.py
@@ -5,6 +5,8 @@ myScale = nau7802py.NAU7802()
 
 grafanaurl="%GRAFANA_URL%"
 
+hiveeyesurl="https://swarm.hiveeyes.org/api/hiveeyes/Kryvle/Zietenaugust/"
+
 if myScale.begin():
     while True:
         currentReading = myScale.getReading()
@@ -16,5 +18,13 @@ if myScale.begin():
             os.system(cmd)
         except:
             print("no access to grafana?")
+            pass
+        time.sleep(1)
+        try:
+            cmd="curl --header \"Content-Type: application/json\" -XPOST -d '{\"weight\": "+str(currentWeight)+"}' "+hiveeyesurl+"Beute2/data"
+            print (cmd)
+            os.system(cmd)
+        except:
+            print("no access to hiveeyes.org?")
             pass
         time.sleep(1)
